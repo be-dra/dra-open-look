1d77f6a73b7fa815ec5fd810efc4cfb4b2c0c638
Bernd Drahota <b-h-dra@t-online.de>
2025-03-02 09:45:11 +0100

    new: ignoreWMHintsIcon

--- olwm/win.h
+++ olwm/win.h
@@ -1,4 +1,4 @@
-/* @(#) win.h V1.5 96/06/10 06:16:14 $Id: win.h,v 2.2 2025/01/11 19:51:15 dra Exp $ */
+/* @(#) win.h V1.5 96/06/10 06:16:14 $Id: win.h,v 2.4 2025/03/01 21:54:15 dra Exp $ */
 /* #ident	"@(#)win.h	26.43	93/06/28 SMI" */
 
 /*
@@ -465,6 +465,7 @@ typedef struct _winiconpane {
 	Pixmap		iconPixmap;		/* None if icon has client window */
 	Pixmap		iconMask;
 	int         iconPixmapDepth; /* the ICCCM said (still says???) depth 1 */
+	Bool		ignoreWMHintsIcon;
 } WinIconPane;
 
 typedef struct _winpushpin {
@@ -486,7 +487,12 @@ typedef struct _winmenu {
 	WinCore		core;
 	struct _menuInfo *menuInfo;
 	Bool		ignoreNextExpose;
-	Window shadow;
+	Window shadow;                   /* used only for the real menu */
+#ifdef REMAP_SHADOW_DID_NOT_WORK
+	Bool        ignoreNextUnobscure; /* only for menu shadows */
+	struct _winmenu *shadowInfo;     /* used only for the real menu */
+	struct _winmenu *realInfo;       /* used only for the shadow */
+#endif /* REMAP_SHADOW_DID_NOT_WORK */
 } WinMenu;
 
 typedef struct _winpinmenu {	/* pinned menus are a subclass of panes */
