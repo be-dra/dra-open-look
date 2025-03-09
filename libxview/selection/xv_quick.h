17e6d7350beaf87d855be75201b9d92b962e8137
Bernd Drahota <b-h-dra@t-online.de>
2025-03-09 09:45:37 +0100

    using const Xv_pkg

--- libxview/selection/xv_quick.h
+++ libxview/selection/xv_quick.h
@@ -2,7 +2,7 @@
 #define xv_quick_h_INCLUDED
 
 /*
- * "@(#) %M% V%I% %E% %U% $Id: xv_quick.h,v 1.4 2025/03/06 16:06:24 dra Exp $"
+ * "@(#) %M% V%I% %E% %U% $Id: xv_quick.h,v 1.6 2025/03/08 14:06:27 dra Exp $"
  *
  * This file is a product of Bernhard Drahota and is provided for
  * unrestricted use provided that this legend is included on all tape
@@ -34,17 +34,15 @@
 #define QUICK_OWNER &xv_quick_owner_pkg
 
 #define	QUICK_ATTR(type, ordinal)	ATTR(ATTR_PKG_QUICK, type, ordinal)
-#define ATTR_QUICK_QUAD ATTR_TYPE(ATTR_BASE_OPAQUE, 4)
 
 typedef	Xv_opaque 		Quick_owner;
 
 typedef enum {
-	QUICK_START_X				= QUICK_ATTR(ATTR_INT,			  1),
-	QUICK_END_X					= QUICK_ATTR(ATTR_INT,			  2),
+	QUICK_START					= QUICK_ATTR(ATTR_OPAQUE_TRIPLE,  1),
+	QUICK_NEED_START			= QUICK_ATTR(ATTR_BOOLEAN,        2),
 	QUICK_BASELINE				= QUICK_ATTR(ATTR_INT,			  3),
-	QUICK_GET_STRING_PROC		= QUICK_ATTR(ATTR_FUNCTION_PTR,	  4),
 	QUICK_REMOVE_UNDERLINE_PROC	= QUICK_ATTR(ATTR_FUNCTION_PTR,	  5),
-	QUICK_SELECT_DOWN			= QUICK_ATTR(ATTR_QUICK_QUAD,     6),
+	QUICK_SELECT_DOWN			= QUICK_ATTR(ATTR_OPAQUE,         6),
 	QUICK_LOC_DRAG				= QUICK_ATTR(ATTR_OPAQUE,         7),
 	QUICK_ADJUST_UP				= QUICK_ATTR(ATTR_OPAQUE,         8),
 	QUICK_FONTINFO				= QUICK_ATTR(ATTR_OPAQUE,         9),
@@ -56,6 +54,6 @@ typedef struct {
     Xv_opaque		private_data;
 } Xv_quick_owner;
 
-extern Xv_pkg 		xv_quick_owner_pkg;
+extern const Xv_pkg 		xv_quick_owner_pkg;
 
 #endif /* xv_quick_h_INCLUDED */
