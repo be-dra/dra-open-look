755fbb0687f36275ca7b7338ebe0f9d38639616a
Bernd Drahota <b-h-dra@t-online.de>
2025-03-09 09:45:37 +0100

    using const Xv_pkg

--- libxview/selection/talk.c
+++ libxview/selection/talk.c
@@ -5,7 +5,7 @@
 #include <xview/talk.h>
 #include <xview/defaults.h>
 
-char talk_c_sccsid[] = "@(#) %M% V%I% %E% %U% $Id: talk.c,v 1.33 2025/03/06 16:06:16 dra Exp $";
+char talk_c_sccsid[] = "@(#) %M% V%I% %E% %U% $Id: talk.c,v 1.35 2025/03/08 14:06:27 dra Exp $";
 
 typedef void (*notify_proc_t)(Talk, char *, int, char **);
 
@@ -24,10 +24,8 @@ typedef enum {
 typedef struct {
 	Xv_opaque       public_self;
 
-	int             i_am_server;
 	Selection_owner selown;
 	Atom            pattern, trigg, regist, deregist, talksrv, targets;
-	/* begin only client */
 	int             last_error;
 	int             silent;
 	int             notify_count;
@@ -35,14 +33,6 @@ typedef struct {
 	Talk_state_t    state;
 	pattern_t       not_yet_installed;
 	pattern_t       installed;
-	/* end only client */
-
-	/* begin only server */
-	pattern_t       allpatterns;
-	time_t          lastreg;
-	int             clnum;
-	long            trigcnt;
-	/* end only server */
 } Talk_private;
 
 #define TALKPRIV(_x_) XV_PRIVATE(Talk_private, Xv_talk, _x_)
@@ -62,21 +52,6 @@ extern char *xv_app_name;
 static int talk_key = 0;
 static void register_to_server(Talk self, Talk_private *priv);
 
-static pattern_t remove_pattern(pattern_t p, Atom sel)
-{
-	if (!p) return p;
-	p->next = remove_pattern(p->next, sel);
-	if (p->client_selection == sel) {
-		pattern_t retval = p->next;
-
-		xv_free(p->name);
-		xv_free(p);
-		return retval;
-	}
-
-	return p;
-}
-
 static int analyze_string(char *str, int sep, char **strings, long max_strings)
 {
 	long i = 0;
@@ -92,197 +67,6 @@ static int analyze_string(char *str, int sep, char **strings, long max_strings)
 
 	strings[i] = NULL;
 	return i;
-}
-
-static int server_convert_proc(Selection_owner sel_own, Atom *type,
-					Xv_opaque *data, unsigned long *length, int *format)
-{
-	Talk_private *priv = (Talk_private *)xv_get(sel_own, XV_KEY_DATA, talk_key);
-
-	if (*type == priv->trigg) {
-		Sel_prop_info *info;
-
-		/* a client wants us to trigger an event, telling us in a string
-		 * what the messagetype is - and the parameters
-		 */
-		if ((info = (Sel_prop_info *)xv_get(sel_own, SEL_PROP_INFO)) &&
-				info->length > 0 &&
-				info->format == 8 &&
-				info->type == XA_STRING)
-		{
-			Talk self = TALKPUB(priv);
-			char *input = (char *)info->data;
-			int inp_len = strlen(input);
-			char tok[100]; /* the message name is not supposed to be longer */
-			char *msg;
-			pattern_t p;
-			int count = 0;
-			char sep[2];
-
-			/*       msg AGS pid AGS params
-			 *  =    msg AGS pid AGS param1 ARS param2 ARS...
-			 */
-			strncpy(tok, input, sizeof(tok) - 2);
-			tok[sizeof(tok) - 2] = '\0';
-			sep[0] = AGS;
-			sep[1] = '\0';
-			msg = strtok(tok, sep);
-			/* now, for every interested client, we attach a copy of input */
-			for (p = priv->allpatterns; p; p = p->next) {
-				if (0 == strcmp(msg, p->name)) {
-					xv_set(self,
-							SEL_RANK, p->client_selection,
-							SEL_TYPE, p->client_selection,
-							SEL_TYPE_INDEX, 0,
-							SEL_PROP_TYPE, XA_STRING,
-							SEL_PROP_FORMAT, 8,
-							SEL_PROP_LENGTH, inp_len,
-							SEL_PROP_DATA, input,
-							NULL);
-					sel_post_req(self);   /* Ref hmvcserfmnb */
-					++count;
-				}
-			}
-			/* how many clients have been 'called' */
-			priv->trigcnt = count;
-			*data = (Xv_opaque)&priv->trigcnt;
-			*length = 1;
-			*format = 32;
-			*type = XA_INTEGER;
-			return TRUE;
-		}
-	}
-	else if (*type == priv->regist) {
-		static Atom clsel;
-		Xv_window owner = xv_get(sel_own, XV_OWNER);
-		Xv_server srv = XV_SERVER_FROM_WINDOW(owner);
-		char namebuf[22];
-		time_t now = time(NULL);
-		int clnum;
-		Display *dpy = (Display *)xv_get(srv, XV_DISPLAY);
-
-		/* originally, we always started with clnum = 0. However,
-		 * when the whole session was started several client registered
-		 * almost at the same time, so, although we gave the first client
-		 * the atom _DRA_TALK0, the second client got the *same* atom,
-		 * because the first did not yet own it...
-		 *
-		 * We remember the time of the last registration
-		 */
-		if (now == priv->lastreg) {
-			priv->clnum++;
-		}
-		else {
-			priv->lastreg = now;
-			priv->clnum = 0;
-		}
-
-		for (clnum = priv->clnum; ; clnum++) {
-			/* a client wants to register - we give him an atom */
-			sprintf(namebuf, "_DRA_TALK%d", clnum);
-			clsel = xv_get(srv, SERVER_ATOM, namebuf);
-			if (None == XGetSelectionOwner(dpy, clsel)) {
-				/* could have been an old client... */
-				priv->allpatterns = remove_pattern(priv->allpatterns, clsel);
-				break;
-			}
-		}
-
-		*data = (Xv_opaque)&clsel;
-		*length = 1;
-		*format = 32;
-		return TRUE;
-	}
-	else if (*type == priv->deregist) {
-		Sel_prop_info *info;
-
-		if ((info = (Sel_prop_info *)xv_get(sel_own, SEL_PROP_INFO)) &&
-				info->length == 1 &&
-				info->format == 32 &&
-				info->type == XA_ATOM)
-		{
-			Atom current = *((Atom *)info->data);
-
-			priv->allpatterns = remove_pattern(priv->allpatterns, current);
-			*data = XV_NULL;
-			*length = 0;
-			*format = 32;
-			return TRUE;
-		}
-	}
-	else if (*type == priv->pattern) {
-		Sel_prop_info *info;
-
-		if ((info = (Sel_prop_info *)xv_get(sel_own, SEL_PROP_INFO)) &&
-				info->length > 0 &&
-				info->format == 8 &&
-				info->type == XA_STRING)
-		{
-			char *inp = (char *)info->data;
-			char *patt;
-			Atom sel;
-			char sep[2];
-			pattern_t p;
-
-			sep[0] = AGS;
-			sep[1] = '\0';
-			/* a client wants to install a pattern - sends his selection
-			 * and a string (for the pattern)
-			 */
-			sel = atoi(strtok(inp, sep));
-			patt = strtok(NULL, sep);
-
-			p = xv_alloc(struct _pattern);
-			p->name = strdup(patt);
-			p->client_selection = sel;
-
-			p->next = priv->allpatterns;
-			priv->allpatterns = p;
-
-			*data = XV_NULL;
-			*length = 0;
-			*format = 8;
-			return TRUE;
-		}
-		return FALSE;
-	}
-	else if (*type == priv->targets) {
-		static Atom tgts[10];
-		int i = 0;
-
-		tgts[i++] = priv->trigg;
-		tgts[i++] = priv->regist;
-		tgts[i++] = priv->deregist;
-		tgts[i++] = priv->pattern;
-		*data = (Xv_opaque)tgts;
-		*type = XA_ATOM;
-		*length = i;
-		*format = 32;
-		return TRUE;
-	}
-
-	return FALSE;
-}
-
-static void note_server_reply(Talk self, Atom target, Atom type,
-					Xv_opaque value, unsigned long length, int format)
-{
-	Talk_private *priv = TALKPRIV(self);
-
-	if (length == SEL_ERROR) {
-		priv->last_error = *((int *)value);
-
-		/* was this the result of the server's trigg request */
-		if (priv->last_error == SEL_BAD_CONVERSION) {
-			/* compare the target to the clients' selection -
-			 * let's get rid of this client
-			 */
-			priv->allpatterns = remove_pattern(priv->allpatterns, target);
-		}
-		return;
-	}
-
-	if (value) xv_free(value);
 }
 
 static Notify_value note_try_again(Talk self, int unused)
@@ -546,7 +330,6 @@ static int talk_init(Xv_window owner, Xv_opaque slf, Attr_avlist avlist, int *u)
 {
 	Xv_talk *self = (Xv_talk *)slf;
 	Talk_private *priv;
-	Attr_attribute *attrs;
 	Xv_server srv = XV_SERVER_FROM_WINDOW(owner);
 
 	if (!(priv = (Talk_private *)xv_alloc(Talk_private))) return XV_ERROR;
@@ -567,11 +350,6 @@ static int talk_init(Xv_window owner, Xv_opaque slf, Attr_avlist avlist, int *u)
 	}
 	xv_set(srv, XV_KEY_DATA, talk_key, slf, NULL);
 
-	for (attrs=avlist; *attrs; attrs=attr_next(attrs)) switch ((int)*attrs) {
-		case TALK_SERVER: priv->i_am_server = (int)A1; ADONE;
-		default: break;
-	}
-
 	priv->talksrv = xv_get(srv, SERVER_ATOM, "_DRA_TALK_SERVER");
 	priv->trigg = xv_get(srv, SERVER_ATOM, "_DRA_TALK_TRIGGER");
 	priv->regist = xv_get(srv, SERVER_ATOM, "_DRA_TALK_REGISTER");
@@ -579,19 +357,7 @@ static int talk_init(Xv_window owner, Xv_opaque slf, Attr_avlist avlist, int *u)
 	priv->pattern = xv_get(srv, SERVER_ATOM, "_DRA_TALK_PATTERN");
 	priv->targets = xv_get(srv, SERVER_ATOM, "TARGETS");
 
-	if (priv->i_am_server) {
-		priv->selown = xv_create(owner, SELECTION_OWNER,
-					XV_KEY_DATA, talk_key, priv,
-					SEL_RANK, priv->talksrv,
-					SEL_CONVERT_PROC, server_convert_proc,
-					SEL_OWN, TRUE,
-					NULL);
-		xv_set(slf, SEL_REPLY_PROC, note_server_reply, NULL);
-	}
-	else {
-		xv_set(slf, SEL_REPLY_PROC, note_client_reply, NULL);
-	}
-
+	xv_set(slf, SEL_REPLY_PROC, note_client_reply, NULL);
 
 	return XV_OK;
 }
@@ -796,7 +562,7 @@ static int talk_destroy(Talk self, Destroy_status status)
 	return XV_OK;
 }
 
-static Xv_object talk_find(Xv_window owner, Xv_pkg *pkg, Attr_avlist avlist)
+static Xv_object talk_find(Xv_window owner, const Xv_pkg *pkg, Attr_avlist avlist)
 {
 	Xv_server srv = XV_SERVER_FROM_WINDOW(owner);
 	Talk self;
@@ -808,7 +574,7 @@ static Xv_object talk_find(Xv_window owner, Xv_pkg *pkg, Attr_avlist avlist)
 	return XV_NULL;
 }
 
-Xv_pkg xv_talk_pkg = {
+const Xv_pkg xv_talk_pkg = {
 	"Talk",
 	ATTR_PKG_TALK,
 	sizeof(Xv_talk),
