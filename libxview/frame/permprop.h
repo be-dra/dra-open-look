8b6477fe3cc810adc822cfb1c94b97bad94da87c
Bernd Drahota <b-h-dra@t-online.de>
2025-03-09 09:45:37 +0100

    using const Xv_pkg

--- libxview/frame/permprop.h
+++ libxview/frame/permprop.h
@@ -2,7 +2,7 @@
 #define permprop_INCLUDED 1
 
 /*
- * "@(#) %M% V%I% %E% %U% $Id: permprop.h,v 4.1 2024/04/12 06:03:17 dra Exp $"
+ * "@(#) %M% V%I% %E% %U% $Id: permprop.h,v 4.2 2025/03/08 13:21:40 dra Exp $"
  *
  * This file is a product of Bernhard Drahota and is provided for
  * unrestricted use provided that this legend is included on all tape
@@ -31,7 +31,7 @@
 #include <xview/frame.h>
 #include <xview/attrol.h>
 
-extern Xv_pkg xv_permprop_pkg;
+extern const Xv_pkg xv_permprop_pkg;
 #define PERMANENT_PROPS &xv_permprop_pkg
 typedef Xv_opaque Perm_prop_frame;
 
