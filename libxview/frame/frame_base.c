d2c27ef2db18cc1b2f2a2591e4e5cf4704638906
Bernd Drahota <b-h-dra@t-online.de>
2025-03-12 09:45:06 +0100

    using const Xv_pkg

--- libxview/frame/frame_base.c
+++ libxview/frame/frame_base.c
@@ -1,6 +1,6 @@
 #ifndef lint
 #ifdef sccs
-static char     sccsid[] = "@(#)frame_base.c 1.31 93/06/28 DRA: $Id: frame_base.c,v 4.3 2024/08/17 19:44:49 dra Exp $ ";
+static char     sccsid[] = "@(#)frame_base.c 1.31 93/06/28 DRA: $Id: frame_base.c,v 4.4 2025/03/08 13:21:40 dra Exp $ ";
 #endif
 #endif
 
@@ -111,7 +111,7 @@ Xv_private void frame_handle_props(Frame frame_public)
 	}
 }
 
-Xv_pkg          xv_frame_base_pkg = {
+const Xv_pkg          xv_frame_base_pkg = {
     "Frame_base", (Attr_pkg) ATTR_PKG_FRAME,
     sizeof(Xv_frame_base),
     &xv_frame_class_pkg,
