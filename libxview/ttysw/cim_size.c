504f6c49203b67709da1a8e325cadcd228c133cd
Bernd Drahota <b-h-dra@t-online.de>
2025-03-20 09:45:10 +0100

    many global variables converted into instance variables

--- libxview/ttysw/cim_size.c
+++ libxview/ttysw/cim_size.c
@@ -1,5 +1,5 @@
 #ifndef lint
-char     cim_size_c_sccsid[] = "@(#)cim_size.c 20.32 93/06/28 DRA: $Id: cim_size.c,v 4.2 2024/12/16 22:52:50 dra Exp $";
+char     cim_size_c_sccsid[] = "@(#)cim_size.c 20.32 93/06/28 DRA: $Id: cim_size.c,v 4.3 2025/03/19 21:33:50 dra Exp $";
 #endif
 
 /*
@@ -34,8 +34,10 @@ char     cim_size_c_sccsid[] = "@(#)cim_size.c 20.32 93/06/28 DRA: $Id: cim_size
 #include <xview_private/charscreen.h>
 #include <xview_private/portable.h>
 
+#ifdef ONLY_ONE_TTY_PER_PROCESS
 CHAR          **image;		/* BUG ALERT! Get rid of this global. */
 char          **screenmode;     /* BUG ALERT! Get rid of this global. */
+#endif /* ONLY_ONE_TTY_PER_PROCESS */
 static CHAR    *lines_ptr;
 static char     *mode_ptr;      /* BUG ALERT! Get rid of this global. */
 static CHAR   **temp_image;
@@ -57,17 +59,17 @@ xv_tty_imageinit(ttysw, window)
 {
     int             maximagewidth, maximageheight;
 
-    if (wininit(window, &maximagewidth, &maximageheight) == 0)
+    if (wininit(ttysw, window, &maximagewidth, &maximageheight) == 0)
 	return (0);
-    ttysw_top = ttysw_left = 0;
-    curscol = ttysw_left;
-    cursrow = ttysw_top;
+    ttysw->ttysw_top = ttysw->ttysw_left = 0;
+    ttysw->curscol = ttysw->ttysw_left;
+    ttysw->cursrow = ttysw->ttysw_top;
     maxright = x_to_col(maximagewidth);
     if (maxright > 255)
 	maxright = 255;		/* line length is stored in a byte */
     maxbottom = y_to_row(maximageheight);
-    (void) xv_tty_imagealloc(ttysw, FALSE);
-    (void) ttysw_pclearscreen(0, ttysw_bottom + 1);	/* +1 to get remnant at
+    xv_tty_imagealloc(ttysw, FALSE);
+    ttysw_pclearscreen(ttysw, 0, ttysw->ttysw_bottom + 1);	/* +1 to get remnant at
 						 * bottom */
     return (1);
 }
@@ -80,66 +82,70 @@ xv_tty_imagealloc(ttysw, for_temp)
     Ttysw          *ttysw;
     int             for_temp;	/* decide which data structure to go into */
 {
-    register CHAR **newimage;
-    register char **newmode;
-    register int    i;
-    int             nchars;
-    register CHAR  *line;
-    register char  *bold;
-    /*
-     * Determine new screen dimensions
-     */
-    ttysw_right = x_to_col(winwidthp);
-    ttysw_bottom = y_to_row(winheightp);
-    /*
-     * Ensure has some non-zero dimension
-     */
-    if (ttysw_right < 1)
-	ttysw_right = 1;
-    if (ttysw_bottom < 1)
-	ttysw_bottom = 1;
-    /*
-     * Bound new screen dimensions
-     */
-    ttysw_right = (ttysw_right < maxright) ? ttysw_right : maxright;
-    ttysw_bottom = (ttysw_bottom < maxbottom) ? ttysw_bottom : maxbottom;
-    /*
-     * Let pty set terminal size
-     */
-    (void) xv_tty_new_size(ttysw, ttysw_right, ttysw_bottom);
-    /*
-     * Allocate line array and character storage
-     */
-    nchars = ttysw_right * ttysw_bottom;
-    newimage = (CHAR **) calloc(1L, (size_t) (ttysw_bottom * sizeof(CHAR *)));
-    newmode = (char **) calloc(1L, ttysw_bottom * sizeof(char *));
-    bold = (char *) calloc(1L, (size_t)(nchars + 2 * ttysw_bottom));
+	register CHAR **newimage;
+	register char **newmode;
+	register int i;
+	int nchars;
+	register CHAR *line;
+	register char *bold;
+
+	/*
+	 * Determine new screen dimensions
+	 */
+	ttysw->ttysw_right = x_to_col(ttysw->winwidthp);
+	ttysw->ttysw_bottom = y_to_row(ttysw->winheightp);
+	/*
+	 * Ensure has some non-zero dimension
+	 */
+	if (ttysw->ttysw_right < 1)
+		ttysw->ttysw_right = 1;
+	if (ttysw->ttysw_bottom < 1)
+		ttysw->ttysw_bottom = 1;
+	/*
+	 * Bound new screen dimensions
+	 */
+	ttysw->ttysw_right = (ttysw->ttysw_right < maxright) ? ttysw->ttysw_right : maxright;
+	ttysw->ttysw_bottom = (ttysw->ttysw_bottom < maxbottom) ? ttysw->ttysw_bottom : maxbottom;
+	/*
+	 * Let pty set terminal size
+	 */
+	(void)xv_tty_new_size(ttysw, ttysw->ttysw_right, ttysw->ttysw_bottom);
+	/*
+	 * Allocate line array and character storage
+	 */
+	nchars = ttysw->ttysw_right * ttysw->ttysw_bottom;
+	newimage = (CHAR **) calloc(1L, (size_t)(ttysw->ttysw_bottom * sizeof(CHAR *)));
+	newmode = (char **)calloc(1L, ttysw->ttysw_bottom * sizeof(char *));
+	bold = (char *)calloc(1L, (size_t)(nchars + 2 * ttysw->ttysw_bottom));
+
 #ifdef OW_I18N
-    line = (CHAR *) calloc(1, (unsigned) ((nchars + 2 * ttysw_bottom) *
-						sizeof(CHAR)));
+	line = (CHAR *) calloc(1, (unsigned)((nchars + 2 * ttysw->ttysw_bottom) *
+					sizeof(CHAR)));
 #else
-    line = (char *) calloc(1L, (size_t) (nchars + 2 * ttysw_bottom));
+	line = (char *)calloc(1L, (size_t)(nchars + 2 * ttysw->ttysw_bottom));
 #endif
-    for (i = 0; i < ttysw_bottom; i++) {
-	newimage[i] = line + 1;
-	newmode[i] = bold + 1;
-	setlinelength(newimage[i], 0);
-	line += ttysw_right + 2;
-	bold += ttysw_right + 2;
-    }
-    if (for_temp) {
-	temp_image = newimage;
-	temp_mode = newmode;
-	temp_lines_ptr = newimage[0] - 1;
-	temp_mode_ptr = newmode[0] - 1;
 
-    } else {
-	image = newimage;
-	screenmode = newmode;
-	lines_ptr = newimage[0] - 1;
-	mode_ptr = newmode[0] - 1;
+	for (i = 0; i < ttysw->ttysw_bottom; i++) {
+		newimage[i] = line + 1;
+		newmode[i] = bold + 1;
+		setlinelength(ttysw, newimage[i], 0);
+		line += ttysw->ttysw_right + 2;
+		bold += ttysw->ttysw_right + 2;
+	}
+	if (for_temp) {
+		temp_image = newimage;
+		temp_mode = newmode;
+		temp_lines_ptr = newimage[0] - 1;
+		temp_mode_ptr = newmode[0] - 1;
 
-    }
+	}
+	else {
+		ttysw->image = newimage;
+		ttysw->screenmode = newmode;
+		lines_ptr = newimage[0] - 1;
+		mode_ptr = newmode[0] - 1;
+
+	}
 }
 
 
@@ -147,8 +153,7 @@ xv_tty_imagealloc(ttysw, for_temp)
  * Free character image.
  */
 /* ARGSUSED */
-Pkg_private void
-xv_tty_free_image_and_mode()
+Pkg_private void xv_tty_free_image_and_mode(Ttysw_private ttysw)
 {
 
 	if (lines_ptr) {
@@ -156,20 +161,20 @@ xv_tty_free_image_and_mode()
 		free((CHAR *) (lines_ptr));
 		lines_ptr = NULL;
 	}
-	if (image) {
-/* 		cfree((CHAR **) image); */
-		free((CHAR **) image);
-		image = NULL;
+	if (ttysw->image) {
+/* 		cfree((CHAR **) ttysw->image); */
+		free((CHAR **) ttysw->image);
+		ttysw->image = NULL;
 	}
 	if (mode_ptr) {
 /* 		cfree((char *)(mode_ptr)); */
 		free((char *)(mode_ptr));
 		mode_ptr = NULL;
 	}
-	if (screenmode) {
-/* 		cfree((char **)screenmode); */
-		free((char **)screenmode);
-		screenmode = NULL;
+	if (ttysw->screenmode) {
+/* 		cfree((char **)ttysw->screenmode); */
+		free((char **)ttysw->screenmode);
+		ttysw->screenmode = NULL;
 	}
 }
 
@@ -179,14 +184,12 @@ xv_tty_free_image_and_mode()
  * Called when screen changes size.  This will let lines get longer (or
  * shorter perhaps) but won't re-fold older lines.
  */
-Pkg_private void
-ttysw_imagerepair(ttysw_view)
-    Ttysw_view_handle ttysw_view;
+Pkg_private void ttysw_imagerepair(Ttysw_view_handle ttysw_view)
 {
-    Ttysw_private     ttysw = TTY_FOLIO_FROM_TTY_VIEW_HANDLE(ttysw_view);
+    Ttysw_private ttysw = TTY_FOLIO_FROM_TTY_VIEW_HANDLE(ttysw_view);
     CHAR          **oldimage;
     register int    oldrow, row;
-    int             oldbottom = ttysw_bottom;
+    int             oldbottom = ttysw->ttysw_bottom;
     int             topstart;
     int             i;
 
@@ -198,15 +201,15 @@ ttysw_imagerepair(ttysw_view)
      * Clear max of old/new screen (not image).
      */
     /*
-     * jcb (void)ttysw_saveCursor(); clrbottom = (oldbottom < ttysw_bottom)?
-     * ttysw_bottom+2: oldbottom+2; (void)ttysw_pclearscreen(0, clrbottom);
+     * jcb (void)ttysw_saveCursor(); clrbottom = (oldbottom < ttysw->ttysw_bottom)?
+     * ttysw->ttysw_bottom+2: oldbottom+2; (void)ttysw_pclearscreen(0, clrbottom);
      * (void)ttysw_restoreCursor();
      *//* Find out where last line of text is (actual oldbottom). */
-    for (row = oldbottom; row > ttysw_top; row--) {
+    for (row = oldbottom; row > ttysw->ttysw_top; row--) {
 #ifdef OW_I18N
-        if (LINE_LENGTH(image[row - 1])) {
+        if (LINE_LENGTH(ttysw->image[row - 1])) {
 #else
-	if (LINE_LENGTH(image[row - 1])) {
+	if (LINE_LENGTH(ttysw->image[row - 1])) {
 #endif
 	    oldbottom = row;
 	    break;
@@ -218,8 +221,8 @@ ttysw_imagerepair(ttysw_view)
      * terminal but aren't notified of changes. However, it should work in
      * many cases  for straight tty programs like the shell.
      */
-    if (oldbottom > ttysw_bottom)
-	topstart = oldbottom - ttysw_bottom;
+    if (oldbottom > ttysw->ttysw_bottom)
+	topstart = oldbottom - ttysw->ttysw_bottom;
     else
 	topstart = 0;
     /*
@@ -227,46 +230,46 @@ ttysw_imagerepair(ttysw_view)
      */
     ttysw->ttysw_lpp = 0;
 
-    oldimage = image;
-    image = temp_image;		/* Hack around globals */
+    oldimage = ttysw->image;
+    ttysw->image = temp_image;		/* Hack around globals */
 
-    for (i = ttysw_top; i < ttysw_bottom; i++)	/* jcb */
-	setlinelength(image[i], 0);
+    for (i = ttysw->ttysw_top; i < ttysw->ttysw_bottom; i++)	/* jcb */
+	setlinelength(ttysw, ttysw->image[i], 0);
 
     /*
-     * jcb	(void)ttysw_cim_clear(ttysw_top, ttysw_bottom); remove extra
+     * jcb	(void)ttysw_cim_clear(ttysw->ttysw_top, ttysw->ttysw_bottom); remove extra
      * repaint #1 jcb
      */
-    /* (This was caused by ~delaypainting" to trigger itimer repaint) */
+    /* (This was caused by ~ttysw_delaypainting" to trigger itimer repaint) */
 
-    image = oldimage;
+    ttysw->image = oldimage;
     oldimage = (CHAR **) 0;
 
     for (oldrow = topstart, row = 0; oldrow < oldbottom; oldrow++, row++) {
-	register int    sl = STRLEN(image[oldrow]);
+	register int    sl = STRLEN(ttysw->image[oldrow]);
 #ifdef	DEBUG_LINELENGTH_WHEN_WRAP
 #ifdef OW_I18N
-        if (sl != LINE_LENGTH(image[oldrow]))
+        if (sl != LINE_LENGTH(ttysw->image[oldrow]))
             printf("real %ld saved %ld, l %ld, oldbottom %ld bottom %ld\n", sl,
-LINE_LENGTH(oldimage[l]), l, oldbottom, ttysw_bottom);
+LINE_LENGTH(oldimage[l]), l, oldbottom, ttysw->ttysw_bottom);
 #else
 	if (sl != LINE_LENGTH(image[oldrow]))
-	    printf("real %ld saved %ld, l %ld, oldbottom %ld bottom %ld\n", sl, LINE_LENGTH(oldimage[l]), l, oldbottom, ttysw_bottom);
+	    printf("real %ld saved %ld, l %ld, oldbottom %ld bottom %ld\n", sl, LINE_LENGTH(oldimage[l]), l, oldbottom, ttysw->ttysw_bottom);
 #endif /* OW_I18N */
 #endif /* DEBUG_LINELENGTH_WHEN_WRAP */
-	if (sl > ttysw_right)
-	    sl = ttysw_right;
+	if (sl > ttysw->ttysw_right)
+	    sl = ttysw->ttysw_right;
 #ifdef OW_I18N
-	XV_BCOPY(image[oldrow], temp_image[row], sl * sizeof(CHAR));
+	XV_BCOPY(ttysw->image[oldrow], temp_image[row], sl * sizeof(CHAR));
 #else
-	XV_BCOPY(image[oldrow], temp_image[row], (size_t)sl);
+	XV_BCOPY(ttysw->image[oldrow], temp_image[row], (size_t)sl);
 #endif
-	XV_BCOPY(screenmode[oldrow], temp_mode[row], (size_t)sl);
-	setlinelength(temp_image[row], sl);
+	XV_BCOPY(ttysw->screenmode[oldrow], temp_mode[row], (size_t)sl);
+	setlinelength(ttysw, temp_image[row], sl);
     }
-    xv_tty_free_image_and_mode();
-    image = temp_image;
-    screenmode = temp_mode;
+    xv_tty_free_image_and_mode(ttysw);
+    ttysw->image = temp_image;
+    ttysw->screenmode = temp_mode;
     lines_ptr = temp_lines_ptr;
     mode_ptr = temp_mode_ptr;
 
@@ -279,6 +282,6 @@ LINE_LENGTH(oldimage[l]), l, oldbottom, ttysw_bottom);
      * column no longer exists (in which case the cursor is placed at the
      * rightmost column).
      */
-    ttysw_pos(curscol, cursrow - topstart);
+    ttysw_pos(ttysw, ttysw->curscol, ttysw->cursrow - topstart);
     /* (void)ttysw_pdisplayscreen(0); 	remove extra repaint #2 jcb */
 }
