4f1dd2145d02cdd519427656ae0cc8e0554de759
Bernd Drahota <b-h-dra@t-online.de>
2025-03-20 09:45:09 +0100

    many global variables converted into instance variables

--- libxview/ttysw/charscreen.h
+++ libxview/ttysw/charscreen.h
@@ -1,4 +1,4 @@
-/*	@(#)charscreen.h 20.12 93/06/28 SMIa DRA: $Id: charscreen.h,v 4.1 2024/03/28 19:24:33 dra Exp $	*/
+/*	@(#)charscreen.h 20.12 93/06/28 SMIa DRA: $Id: charscreen.h,v 4.2 2025/03/19 21:33:50 dra Exp $	*/
 
 /*
  *	(c) Copyright 1989 Sun Microsystems, Inc. Sun design patents
@@ -13,35 +13,29 @@
 /*
  * Macros to convert character coordinates to pixel coordinates.
  */
-#define row_to_y(row)	((row)*chrheight)
-#define col_to_x(col)	(((col)*chrwidth) + chrleftmargin)
-#define y_to_row(y)	((y)/chrheight)
-#define x_to_col(x)	((((x) >= chrleftmargin) ? \
-			  ((x) - chrleftmargin) : 0)/chrwidth)
+#define row_to_y(row)	((row)*ttysw->chrheight)
+#define col_to_x(col)	(((col)*ttysw->chrwidth) + ttysw->chrleftmargin)
+#define y_to_row(y)	((y)/ttysw->chrheight)
+#define x_to_col(x)	((((x) >= ttysw->chrleftmargin) ? \
+			  ((x) - ttysw->chrleftmargin) : 0)/ttysw->chrwidth)
 
 /*
  * Character dimensions (fixed width fonts only!)
  * and of screen in pixels.
  */
-Xv_private int	chrheight, chrwidth, chrbase;
-Xv_private int	winheightp, winwidthp;
-Xv_private int	chrleftmargin;
-
-Xv_private struct	pixfont *pixfont;
 
 /*
- * If delaypainting, delay painting.  Set when clear screen.
+ * If ttysw_delaypainting, delay painting.  Set when clear screen.
  * When input will block then paint characters (! white space) of entire image
- * and turn delaypainting off.
+ * and turn ttysw_delaypainting off.
  */
-Xv_private int	delaypainting;
+Xv_private int	ttysw_delaypainting;
 
-Pkg_private void ttysw_pstring(CHAR *s,
-				/* dieser Compiler warnt immer ueber 'different width'
-				 * bei formalen Parametern von Typ char....
-				 */
+struct ttysubwindow;
+
+Pkg_private void ttysw_pstring(struct ttysubwindow *ttysw, CHAR *s,
 				int mode,
 				int col, int row, int op);
-void	ttysw_pclearline(int fromcol, int tocol, int row);
-void	ttysw_pcopyline(int fromcol, int tocol, int count, int row);
-Pkg_private void ttysw_pclearscreen( int fromrow, int torow);
+Pkg_private void ttysw_pclearline(struct ttysubwindow *ttysw, int fromcol, int tocol, int row);
+Pkg_private void ttysw_pcopyline(struct ttysubwindow *ttysw, int fromcol, int tocol, int count, int row);
+Pkg_private void ttysw_pclearscreen(struct ttysubwindow *ttysw,int fromrow, int torow);
