39d86cbe4c080967ca70ce65099f2e0459ca80d0
Bernd Drahota <b-h-dra@t-online.de>
2025-03-30 09:45:12 +0200

    warning eliminated

--- libxview/notify/nintievent.c
+++ libxview/notify/nintievent.c
@@ -1,6 +1,6 @@
 #ifndef	lint
 #ifdef sccs
-static char     sccsid[] = "@(#)nintievent.c 20.12 93/06/28 Copyr 1985 Sun Micro  DRA: $Id: nintievent.c,v 4.1 2024/03/28 18:09:08 dra Exp $ ";
+static char     sccsid[] = "@(#)nintievent.c 20.12 93/06/28 Copyr 1985 Sun Micro  DRA: $Id: nintievent.c,v 4.2 2025/03/29 21:02:24 dra Exp $ ";
 #endif
 #endif
 
@@ -18,7 +18,8 @@ static char     sccsid[] = "@(#)nintievent.c 20.12 93/06/28 Copyr 1985 Sun Micro
 #include <xview_private/ndet.h>
 #include <xview_private/nint.h>
 
-Notify_error notify_interpose_event_func(Notify_client nclient, Notify_event_interposer_func func, Notify_event_type when)
+Notify_error notify_interpose_event_func(Notify_client nclient,
+				Notify_event_interposer_func func, Notify_event_type when)
 {
 	NTFY_TYPE type;
 
@@ -26,6 +27,6 @@ Notify_error notify_interpose_event_func(Notify_client nclient, Notify_event_int
 	if (ndet_check_when(when, &type))
 		return notify_errno;
 
-	return (nint_interpose_func(nclient, func, type, NTFY_DATA_NULL,
-					NTFY_IGNORE_DATA));
+	return nint_interpose_func(nclient, (Notify_func)func, type, NTFY_DATA_NULL,
+					NTFY_IGNORE_DATA);
 }
