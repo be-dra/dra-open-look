5a539c6802401af483f4060257cb7c63cb2a3679
Bernd Drahota <b-h-dra@t-online.de>
2025-03-30 09:45:12 +0200

    renamed field an_u_int to an_u_long

--- libxview/notify/ntfy_cond.c
+++ libxview/notify/ntfy_cond.c
@@ -1,6 +1,6 @@
 #ifndef	lint
 #ifdef sccs
-static char     sccsid[] = "@(#)ntfy_cond.c 20.19 93/06/28 Copyr 1985 Sun Micro  DRA: $Id: ntfy_cond.c,v 4.1 2024/03/28 18:09:08 dra Exp $ ";
+static char     sccsid[] = "@(#)ntfy_cond.c 20.19 93/06/28 Copyr 1985 Sun Micro  DRA: $Id: ntfy_cond.c,v 4.2 2025/03/29 21:01:26 dra Exp $ ";
 #endif
 #endif
 
@@ -31,73 +31,67 @@ static int ntfy_paranoid_count;
 static NTFY_CONDITION *ntfy_enum_condition;
 static NTFY_CONDITION *ntfy_enum_condition_next;
 
-pkg_private NTFY_CONDITION *
-ntfy_find_condition(condition_list, type, condition_latest, data, use_data)
-    NTFY_CONDITION *condition_list;
-    NTFY_TYPE       type;
-    register NTFY_CONDITION **condition_latest;
-    NTFY_DATA       data;
-    int             use_data;
+void ntfy_reset_paranoid(void);
+
+pkg_private NTFY_CONDITION *ntfy_find_condition(NTFY_CONDITION *condition_list,
+				NTFY_TYPE type, register NTFY_CONDITION **condition_latest,
+				NTFY_DATA data, int use_data)
 {
-    register NTFY_CONDITION *condition;
-    NTFY_CONDITION *next;
+	register NTFY_CONDITION *condition;
+	NTFY_CONDITION *next;
 
-    ntfy_assert(NTFY_IN_CRITICAL, 23 /* Unprotected list search */);
-    /* See if hint matches */
-    if (*condition_latest && (*condition_latest)->type == type) {
-	if (!use_data ||
-	    (NTFY_DATA) (*condition_latest)->data.an_u_int == data)
-	    return (*condition_latest);
-    }
-    /* Search entire list */
-    for (condition = condition_list; condition; condition = next) {
-	next = condition->next;
-	if (condition->type == type) {
-	    if (!use_data ||
-		(NTFY_DATA) condition->data.an_u_int == data) {
-		/* Set up hint for next time */
-		*condition_latest = condition;
-		return (condition);
-	    }
+	ntfy_assert(NTFY_IN_CRITICAL, 23 /* Unprotected list search */ );
+	/* See if hint matches */
+	if (*condition_latest && (*condition_latest)->type == type) {
+		if (!use_data || (NTFY_DATA) (*condition_latest)->data.an_u_long == data)
+			return (*condition_latest);
 	}
-    }
+	/* Search entire list */
+	for (condition = condition_list; condition; condition = next) {
+		next = condition->next;
+		if (condition->type == type) {
+			if (!use_data || (NTFY_DATA) condition->data.an_u_long == data) {
+				/* Set up hint for next time */
+				*condition_latest = condition;
+				return (condition);
+			}
+		}
+	}
 
-    return (NTFY_CONDITION_NULL);
+	return (NTFY_CONDITION_NULL);
 }
 
 /*
  * Find/create condition defined by type and data (if use_data TRUE)
  */
-pkg_private NTFY_CONDITION *
-ntfy_new_condition(condition_list, type, condition_latest, data, use_data)
-    NTFY_CONDITION **condition_list;
-    NTFY_TYPE       type;
-    NTFY_CONDITION **condition_latest;
-    NTFY_DATA       data;
-    int             use_data;
+pkg_private NTFY_CONDITION *ntfy_new_condition(NTFY_CONDITION **condition_list,
+							NTFY_TYPE type, NTFY_CONDITION **condition_latest,
+							NTFY_DATA data, int use_data)
 {
-    register NTFY_CONDITION *condition;
+	register NTFY_CONDITION *condition;
 
-    if ((condition = ntfy_find_condition(*condition_list, type,
-		condition_latest, data, use_data)) == NTFY_CONDITION_NULL) {
-	/* Allocate condition */
-	if ((condition = ntfy_alloc_condition()) == NTFY_CONDITION_NULL)
-	    return (NTFY_CONDITION_NULL);
-	/* Initialize condition */
-	condition->next = NTFY_CONDITION_NULL;
-	condition->type = type;
-	condition->data.an_u_int = (u_int) ((use_data) ? data : 0);
-	condition->callout.function = (Notify_func)notify_nop;
-	condition->func_count = 0;
-	condition->func_next = 0;
-	condition->arg = (Notify_arg) 0;
-	condition->release = NOTIFY_RELEASE_NULL;
-	/* Append to condition list */
-	ntfy_append_condition(condition_list, condition);
-	/* Set up condition hint */
-	*condition_latest = condition;
-    }
-    return (condition);
+	if ((condition = ntfy_find_condition(*condition_list, type,
+							condition_latest, data,
+							use_data)) == NTFY_CONDITION_NULL)
+	{
+		/* Allocate condition */
+		if ((condition = ntfy_alloc_condition()) == NTFY_CONDITION_NULL)
+			return (NTFY_CONDITION_NULL);
+		/* Initialize condition */
+		condition->next = NTFY_CONDITION_NULL;
+		condition->type = type;
+		condition->data.an_u_long = (u_long) ((use_data) ? data : 0);
+		condition->callout.function = (Notify_func) notify_nop;
+		condition->func_count = 0;
+		condition->func_next = 0;
+		condition->arg = (Notify_arg) 0;
+		condition->release = NOTIFY_RELEASE_NULL;
+		/* Append to condition list */
+		ntfy_append_condition(condition_list, condition);
+		/* Set up condition hint */
+		*condition_latest = condition;
+	}
+	return (condition);
 }
 
 pkg_private void
@@ -129,16 +123,16 @@ ntfy_remove_condition(client, condition, who)
 	ntfy_remove_from_table(client, condition);
 	/* Free data portion of condition if dynamically allocated */
 	/* First, check to see if might be non-null pointer */
-	if (condition->data.an_u_int != 0) {
+	if (condition->data.an_u_long != 0) {
 		switch (condition->type) {
 			case NTFY_REAL_ITIMER:
 			case NTFY_VIRTUAL_ITIMER:
 				if (who == NTFY_NDET)
-					ntfy_free_node((NTFY_NODE *) condition->data.an_u_int);
+					ntfy_free_node((NTFY_NODE *) condition->data.an_u_long);
 				break;
 			case NTFY_WAIT3:
 				if (who == NTFY_NDIS)
-					ntfy_free_malloc((NTFY_DATA) condition->data.an_u_int);
+					ntfy_free_malloc((NTFY_DATA) condition->data.an_u_long);
 				break;
 			default:
 				break;
@@ -239,8 +233,7 @@ Notify_value notify_nop()
     return (NOTIFY_IGNORED);
 }
 
-void
-ntfy_reset_paranoid()
+void ntfy_reset_paranoid(void)
 {
     /* Reset static variables used in paranoid enumerator */
     ntfy_paranoid_count = 0;
