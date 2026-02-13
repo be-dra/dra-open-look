98b81905e9a0672fbc9246df9cd63e6a77ad1336
Bernd Drahota <b-h-dra@t-online.de>
2026-02-13 10:17:10 +0100

    cms_impl.h is back - was needed in pw/xv_rop.c

--- libxview/color/cms.c
+++ libxview/color/cms.c
@@ -1,6 +1,6 @@
 #ifndef lint
 #ifdef sccs
-static char     sccsid[] = "@(#)cms.c 1.17 91/03/18 DRA: RCS $Id: cms.c,v 2.3 2026/02/09 14:25:53 dra Exp $";
+static char     sccsid[] = "@(#)cms.c 1.17 91/03/18 DRA: RCS $Id: cms.c,v 2.4 2026/02/12 11:01:17 dra Exp $";
 #endif
 #endif
 
@@ -9,9 +9,8 @@ static char     sccsid[] = "@(#)cms.c 1.17 91/03/18 DRA: RCS $Id: cms.c,v 2.3 20
 #include <X11/Xatom.h>
 #include <xview_private/i18n_impl.h>
 #include <xview_private/scrn_vis.h>
-#include <xview/cms.h>
+#include <xview_private/cms_impl.h>
 #include <xview/server.h>
-#include <xview/screen.h>
 #include <xview/defaults.h>
 #include <olgx/olgx.h>
 
@@ -20,55 +19,6 @@ static char     sccsid[] = "@(#)cms.c 1.17 91/03/18 DRA: RCS $Id: cms.c,v 2.3 20
  *      pending in the U.S. and foreign countries. See LEGAL_NOTICE
  *      file for terms of the license.
  */
-
-/*
- ***********************************************************************
- *		Typedefs, enumerations, and structs
- ***********************************************************************
- */
-
-typedef struct xv_colormap {
-    /* BUG: should have a default for the visual field */
-    Colormap	    	id;
-    Cmap_type		type;
-    struct cms_info 	*cms_list;
-    struct xv_colormap  *next;
-} Xv_Colormap;
-
-#define BIT_FIELD(field)             unsigned field : 1
-#define XV_INVALID_PIXEL	-1
-
-
-typedef struct cms_info {
-    Cms			public_self;
-    char	       *name;
-    Cms_type		type;
-    unsigned 	size;
-    unsigned long      *index_table;
-    Xv_Colormap	       *cmap;
-    Xv_Screen           screen;
-    struct screen_visual *visual;
-    struct cms_info    *next;
-    struct {
-	BIT_FIELD(default_cms);
-	BIT_FIELD(frame_cms);
-	BIT_FIELD(control_cms);
-    } status_bits;
-} Cms_info;
-
-#define STATUS(cms, field)           ((cms)->status_bits.field)
-#define STATUS_SET(cms, field)       STATUS(cms, field) = TRUE
-#define STATUS_RESET(cms, field)     STATUS(cms, field) = FALSE
-
-#define CMS_PRIVATE(cms_public)	XV_PRIVATE(Cms_info, Xv_cms_struct, cms_public)
-#define CMS_PUBLIC(cms)		XV_PUBLIC(cms)
-
-#define XV_CMS_BACKGROUND(cms) (cms)->index_table[0]
-#define XV_CMS_FOREGROUND(cms) (cms)->index_table[(cms)->size - 1]
-
-#define XV_TO_X_PIXEL(index, cms) \
-        (cms)->index_table[((index) >= (cms)->size) ? (cms)->size - 1:(index)]
-
 #define XV_CMS_IS_STATIC(cms)	(cms->type == XV_STATIC_CMS) ? TRUE : FALSE
 #define XV_CMS_IS_DEFAULT(cms)	strcmp(cms->name, XV_DEFAULT_CMS) ? FALSE : TRUE
 #define XV_DYNAMIC_VISUAL(class) ((class) % 2)
