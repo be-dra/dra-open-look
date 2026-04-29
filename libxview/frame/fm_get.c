9d0d5e1834d0c35b2f1c7a4660020205df834bd9
Bernd Drahota <b-h-dra@t-online.de>
2026-04-29 10:17:05 +0200

    many function concentrated in frame_init.c

---
+++ libxview/frame/fm_get.c
@@ -0,0 +1,17 @@
+#ifndef lint
+#ifdef sccs
+static char     sccsid[] = "@(#)fm_get.c 20.62 93/06/28 DRA: $Id: fm_get.c,v 4.6 2026/04/18 06:06:17 dra Exp $ ";
+#endif
+#endif
+
+/*
+ *	(c) Copyright 1989 Sun Microsystems, Inc. Sun design patents 
+ *	pending in the U.S. and foreign countries. See LEGAL_NOTICE 
+ *	file for terms of the license.
+ */
+
+#include <xview_private/fm_impl.h>
+#include <xview_private/draw_impl.h>
+#include <xview_private/win_info.h>
+#include <xview/server.h>
+
