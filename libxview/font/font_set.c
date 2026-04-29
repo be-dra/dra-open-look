7d4dd9759f0b6dcae5ad01397129f26a83d6bafd
Bernd Drahota <b-h-dra@t-online.de>
2026-04-29 10:17:06 +0200

    get and set methods noe static in font.c

---
+++ libxview/font/font_set.c
@@ -0,0 +1,21 @@
+#ifndef lint
+#ifdef sccs
+static char     sccsid[] = "@(#)font_set.c 20.28 93/06/28 DRA: RCS $Id: font_set.c,v 4.3 2026/04/27 18:50:09 dra Exp $ ";
+#endif
+#endif
+
+/*
+ *	(c) Copyright 1989 Sun Microsystems, Inc. Sun design patents
+ *	pending in the U.S. and foreign countries. See LEGAL NOTICE
+ *	file for terms of the license.
+ */
+
+#ifdef OW_I18N
+#include <euc.h>
+#endif /* OW_I18N */
+#include <xview_private/i18n_impl.h>
+#include <xview_private/font_impl.h>
+
+/*
+ * Private
+ */
