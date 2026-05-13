fb4c1355e898565371bd4ded6d9f450c8cca855b
Bernd Drahota <b-h-dra@t-online.de>
2026-05-13 16:16:49 +0200

    all concentrated in dnd.c

--- libxview/dnd/dndutil.c
+++ libxview/dnd/dndutil.c
@@ -1,6 +1,6 @@
 #ifndef lint
 #ifdef sccs
-static char     sccsid[] = "@(#)dndutil.c 1.16 93/06/28 DRA: $Id: dndutil.c,v 4.6 2026/05/11 19:33:30 dra Exp $ ";
+static char     sccsid[] = "@(#)dndutil.c 1.16 93/06/28 DRA: $Id: dndutil.c,v 4.7 2026/05/13 13:51:27 dra Exp $ ";
 #endif
 #endif
 
@@ -10,48 +10,8 @@ static char     sccsid[] = "@(#)dndutil.c 1.16 93/06/28 DRA: $Id: dndutil.c,v 4.
  *      file for terms of the license.
  */
 
-#include <X11/Xproto.h>
 #include <xview/xview.h>
 #include <xview/cursor.h>
 #include <xview/server.h>
 #include <xview/dragdrop.h>
 #include <xview_private/dndimpl.h>
-
-static int sendEventError;
-static XErrorHandler old_handler;
-
-static int sendEventErrorHandler(Display *dpy, XErrorEvent *error)
-{
-	if (error->request_code == X_SendEvent) {
-		sendEventError = True;
-		return 0;
-	}
-	return (*old_handler) (dpy, error);
-}
-
-int debug_DND = -1;
-
-Pkg_private int DndSendEvent(Display *dpy, XEvent *event, const char *nam)
-{
-    Status status;
-
-	if (debug_DND < 0) {
-		char *envdnddeb = getenv("XVDND_DEBUG");
-
-		if (envdnddeb && *envdnddeb) debug_DND = atoi(envdnddeb);
-		else debug_DND = 0;
-	}
-    sendEventError = False;
-    old_handler = XSetErrorHandler(sendEventErrorHandler);
-
-    status = XSendEvent(dpy, event->xbutton.window, False, NoEventMask,
-			(XEvent *) event);
-	if (debug_DND > 0) fprintf(stderr, "sent %s, before XSync\n", nam);
-    XFlush(dpy);
-    (void) XSetErrorHandler(old_handler);
-	if (debug_DND > 0) fprintf(stderr, "              after XSync\n");
-
-    if (status && ! sendEventError) return DND_SUCCEEDED;
-    
-    return DND_ERROR;
-}
