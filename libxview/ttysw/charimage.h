bf9053ef41262b46184119cd577734c2325004e9
Bernd Drahota <b-h-dra@t-online.de>
2026-08-05 10:17:28 +0200

    intermediate state which can (at least) display a file with all german umlauts

--- libxview/ttysw/charimage.h
+++ libxview/ttysw/charimage.h
@@ -1,4 +1,4 @@
-/*	@(#)charimage.h 20.14 93/06/28 SMI RCS: $Id: charimage.h,v 4.4 2026/07/30 12:06:21 dra Exp $	*/
+/*	@(#)charimage.h 20.14 93/06/28 SMI RCS: $Id: charimage.h,v 4.5 2026/08/04 18:21:38 dra Exp $	*/
 
 /*
  *	(c) Copyright 1989 Sun Microsystems, Inc. Sun design patents
@@ -10,27 +10,23 @@
  * Definitions relating to maintenance of virtual screen image.
  */
 
-/*
- * Screen is maintained as an array of characters.
- * Screen is bottom lines and right columns.
- * Each line has length and array of characters.
- * Characters past length position are undefined.
- * Line is otherwise null terminated.
- */
-#ifdef ONLY_ONE_TTY_PER_PROCESS
-extern CHAR	**image;
-extern char	**screenmode;
-extern int	ttysw_top, ttysw_bottom, ttysw_left, ttysw_right;
-extern int	cursrow, curscol;
-#endif /* ONLY_ONE_TTY_PER_PROCESS */
+/* also in the multibyte case: this is the line length IN BYTES ! */
+#define LINE_LENGTH(line)	syntax error
+#define LINE_LENGTH_BYTES(line)	((unsigned char)((line)[-1]))
+#define LINE_LENGTH_CHARS(line)	((unsigned char)((line)[-2]))
 
-#define LINE_LENGTH(line)	((unsigned char)((line)[-1]))
 #define MODE_CLEAR	0
 #define MODE_INVERT	1
 #define MODE_UNDERSCORE	2
 #define MODE_BOLD	4
 
+#ifdef BEFORE_MULTIBYTE
 #define	setlinelength(ttysw, line, column) \
 	{ int _col = ((column)>ttysw->ttysw_right)?ttysw->ttysw_right:(column); \
 	  (line)[(_col)] = '\0'; \
 	  line[-1] = (unsigned char) (_col);}
+#else
+Pkg_private int column_to_byteoffset(const char *s, int col);
+Pkg_private void set_linelength(Ttysw_private ttysw, char *line, int column);
+#define	setlinelength(ttysw, line, column) set_linelength(ttysw, line, column)
+#endif
